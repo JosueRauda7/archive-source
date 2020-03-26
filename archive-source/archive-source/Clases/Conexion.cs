ae84e606e9337fa1be092c998e4739bd444b9636
ivanboot <30850852+ivanboot@users.noreply.github.com>
2020-03-26 12:03:02 -0600

    Clase conexion

---
+++ archive-source/archive-source/Clases/Conexion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace archive_source.Clases
+{
+    class Conexion
+    {
+        //Cadena de conexión con la bdd
+        static string cadena = "SERVER=127.0.0.1;PORT=3306;DATABASE=archive-source;UID=root;PASSWORDS=;";
+        MySqlConnection cn = new MySqlConnection(cadena);
+
+        public void abrirConexion()
+        {
+            cn.Open();
+        }
+
+        public void cerrarConexion()
+        {
+            cn.Close();
+        }
+
+        //Función para probar que puedo seleccionar administradores de la bdd
+        public void prueba()
+        {
+            cn.Open();
+            string consulta = "Select * from main";
+
+            MySqlCommand comando = new MySqlCommand(consulta, cn);
+            MySqlDataReader lector = comando.ExecuteReader();
+            while (lector.Read())
+            {
+                MessageBox.Show("-------------Resultados--------------\n" +
+                                "id: " + lector.GetValue(0) + "\n" +
+                                "usuario: " + lector.GetValue(1) + " \n" +
+                                "Nombre: " + lector.GetValue(2) + "\n" +
+                                "Contra:" + lector.GetValue(3));
+            }
+            cn.Close();
+        }
+    }
+}
---
+++ archive-source/archive-source/Conexion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace archive_source
+{
+    class Conexion
+    {
+        static string cadena = "SERVER=127.0.0.1;PORT=3306;DATABASE=bdd_ads;UID=root;PASSWORDS=;";
+        MySqlConnection cn = new MySqlConnection(cadena);
+
+        public void abrirConexion()
+        {
+            cn.Open();
+        }
+
+        public void cerrarConexion()
+        {
+            cn.Close();
+        }
+
+        public void prueba()
+        {
+            cn.Open();
+            string consulta = "Select * from main";
+            
+            MySqlCommand comando = new MySqlCommand(consulta,cn);
+            MySqlDataReader lector = comando.ExecuteReader();
+            while (lector.Read())
+            {
+                MessageBox.Show("-------------Resultados--------------\n" +
+                                "id: "+lector.GetValue(0).ToString()+ "\n" +
+                                "usuario: "+lector.GetValue(1) + " \n" +
+                                "Nombre: "+lector.GetValue(2)+"\n" +
+                                "Contra:"+lector.GetValue(3));
+            }
+            cn.Close();
+        }
+    }
+}
